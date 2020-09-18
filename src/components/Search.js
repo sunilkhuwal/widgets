74903ebb66b4954541517a230634b876bbdea490
khuwals <sunil.khuwal@gmail.com>
2020-09-19 02:08:30 +0530

    Search api integrated

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,64 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const Search = () => {
   const [term, setTerm] = useState("");
-  return (
-    <div className="ui form">
-      <div className="field">
-        <label>Enter Search Term</label>
-        <input
-          value={term}
-          onChange={(e) => setTerm(e.target.value)}
-          className="input"
-        />
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+
+    if (term) {
+      search();
+    }
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="ui right floating">
+          <a
+            className="ui button"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }} />{" "}
+          {/* don't use this ever as it will open up XSS */}
+        </div>
       </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+      <div className="">{renderedResults}</div>
     </div>
   );
 };
